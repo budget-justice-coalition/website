d449b6d59a6e804f1f89179ab01fdc4a81962709
Shailen <naidooshailen648@gmail.com>
2019-06-01 14:56:53 +0200

    Added resources link to navbar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,12 @@
 module.exports = {
   title: 'Budget Justice',
-  description: 'The purpose of the Budget Justice Coalition (BJC) is to collaboratively build people\'s participation in and understanding of South Africa\'s budget and planning processes.'
+  description: 'The purpose of the Budget Justice Coalition (BJC) is to collaboratively build people\'s participation in and understanding of South Africa\'s budget and planning processes.',
+  themeConfig: {
+    nav: [
+      {
+        text: 'Resources',
+        link: '/resources/'
+      }
+    ]
+  }
 }
