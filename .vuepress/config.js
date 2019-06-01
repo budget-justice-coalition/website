0b82ee8dcb296b7a9755369ddac3e1c0beae1d84
Shailen <naidooshailen648@gmail.com>
2019-06-01 16:11:06 +0200

    Humanized homepage description

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'Budget Justice',
-  description: 'The purpose of the Budget Justice Coalition (BJC) is to collaboratively build people\'s participation in and understanding of South Africa\'s budget and planning processes.',
+  description: 'Collaboratively building South Africa\'s budget and planning processes',
   themeConfig: {
     nav: [
       {
