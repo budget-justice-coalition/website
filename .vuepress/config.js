3d4e41e5b502b8f11e8c30e49c532b949abeef97
Shailen Naidoo <shailen@gettruck.co.za>
2019-06-13 20:01:23 +0200

    Added montserrat font

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,9 @@
 module.exports = {
   title: 'Budget Justice',
   description: 'Collaboratively building South Africa\'s budget and planning processes',
+  head: [
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat&display=swap' }],
+  ],
   themeConfig: {
     nav: [
       {
