4c0db80274ed2ac91f160f61c99f0b7885f66131
Shailen <shailen@gettruck.co.za>
2019-06-28 15:14:40 +0200

    Added sw loading script

---
+++ plugins/load-sw.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-console
+console.log('Loading service worker!')
+
+const loadSW = () => {
+  if (!('serviceWorker' in navigator)) {
+    return false
+  }
+
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      // eslint-disable-next-line no-console
+      .then(() => console.log('Service worker registered successfully'))
+      // eslint-disable-next-line no-console
+      .catch(() => console.log('Error loading service worker'))
+  })
+}
+
+loadSW()
