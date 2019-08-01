b5e573737d6ec85b96b9601b6e1d1d96401a7e2c
Shailen Naidoo <naidooshailen648@gmail.com>
2019-08-01 17:38:40 +0200

    Minor changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    { src: '@/plugins/load-sw.js', ssr: false }
+    // { src: '@/plugins/load-sw.js', ssr: false }
   ],
   /*
   ** Nuxt.js modules
